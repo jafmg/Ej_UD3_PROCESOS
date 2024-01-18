ed4dfb886bd3acd70985553f00c4ca2fa388106d
Jose Antonio Fdez-Montes Garcia <jafmgdam@gmail.com>
2024-01-18 17:15:28 +0100

    18/01/24

--- Ud3Sockets/src/ejercicios/Ej1Cliente.java
+++ Ud3Sockets/src/ejercicios/Ej1Cliente.java
@@ -1,23 +1,38 @@
 package ejercicios;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Ej1Cliente {
 
-	final static String HOST = "10.11.0.206"; 
+	final static String HOST = "192.168.1.36"; 
 	final static int PORT = 5000;
 	
 	public static void main(String[] args) {
 		
 		DataInputStream in;
-		DataInputStream out;
+		DataOutputStream out;
 		
 		try {
 			Socket sc = new Socket(HOST, PORT);
 			System.out.println("Cliente conectado");
+			
+			in = new DataInputStream(sc.getInputStream());
+			out = new DataOutputStream(sc.getOutputStream());
+			
+			String mensaje = in.readUTF();
+			System.out.println("Mensaje del servidor: " + mensaje);
+			mensaje.toLowerCase();
+			mensaje = mensaje.toLowerCase();
+			out.writeUTF(mensaje);
+			
+			sc.close();
+			in.close();
+			out.close();
+			
 			
 			
 			
--- Ud3Sockets/src/ejercicios/Ej1Servidor.java
+++ Ud3Sockets/src/ejercicios/Ej1Servidor.java
@@ -1,7 +1,10 @@
 package ejercicios;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Ej1Servidor {
 
@@ -10,7 +13,25 @@ public class Ej1Servidor {
 	public static void main(String[] args) {
 		try {
 			ServerSocket servidor = new ServerSocket(PUERTO);
+			DataInputStream in;
+			DataOutputStream out;
+			Socket sc = null;
 			System.out.println("Inicializado el servidor");
+			
+			while(true) {
+				sc = servidor.accept();
+				System.out.println("Conexion establecida");
+				in = new DataInputStream(sc.getInputStream());
+				out = new DataOutputStream(sc.getOutputStream());
+				
+				out.writeUTF("HOLA QUE TAL");
+				System.out.println("Mensaje del cliente " + in.readUTF());
+				
+				in.close();
+				out.close();
+				System.out.println("Cliente desconectado");
+				
+			}
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
